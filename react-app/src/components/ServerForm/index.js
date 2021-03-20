b93c47348ad8e432a533a081758493d43fd882cf
nkuek <nkuek1@gmail.com>
2021-03-20 18:02:49 -0400

    server form modal skeleton complete

--- react-app/src/components/ServerForm/index.js
+++ react-app/src/components/ServerForm/index.js
@@ -1,8 +1,8 @@
-import React, { useState, useCallback, useRef } from 'react';
+import React, { useState, useRef, useEffect, useCallback } from 'react';
+import { useSpring, animated } from 'react-spring';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { createServer } from '../../store/server';
-import Modal from 'react-modal';
 import './ServerForm.css';
 
 function ServerForm({ showServerModal, setShowServerModal }) {
@@ -15,12 +15,26 @@ function ServerForm({ showServerModal, setShowServerModal }) {
     const serverModalRef = useRef();
 
     // close modal when clicking anywhere else
-    // does not seem to work when clicking on navbar yet
     const closeServerModal = (e) => {
         if (serverModalRef.current === e.target) {
             setShowServerModal(false);
         }
     };
+
+    // close modal when pressing escape key
+    const keyPress = useCallback(
+        (e) => {
+            if (e.key == 'Escape' && showServerModal) {
+                setShowServerModal(false);
+            }
+        },
+        [showServerModal, setShowServerModal]
+    );
+
+    useEffect(() => {
+        document.addEventListener('keydown', keyPress);
+        return () => document.removeEventListener('keydown', keyPress);
+    }, [keyPress]);
 
     const onSubmit = async (e) => {
         e.preventDefault();
@@ -33,8 +47,18 @@ function ServerForm({ showServerModal, setShowServerModal }) {
                 image,
             })
         );
+        setShowServerModal(false);
         history.push(`/servers/${newServer.id}`);
     };
+
+    // Modal animations from react-spring
+    const animation = useSpring({
+        config: {
+            duration: 150,
+        },
+        opacity: showServerModal ? 1 : 0,
+        transform: showServerModal ? `scale(1)` : `scale(0.8)`,
+    });
 
     return showServerModal ? (
         <div
@@ -42,55 +66,64 @@ function ServerForm({ showServerModal, setShowServerModal }) {
             ref={serverModalRef}
             onClick={closeServerModal}
         >
-            <div className="serverModalContainer">
-                <div className="serverModalFormTitleContainer">
-                    <div className="serverModalFormTitle">Create a Server</div>
+            <animated.div style={animation}>
+                <div className="serverModalContainer">
+                    <div className="serverModalFormTitleContainer">
+                        <div className="serverModalFormTitle">
+                            Create a Server
+                        </div>
+                    </div>
+                    <form
+                        className="serverModalForm"
+                        onSubmit={(e) => onSubmit(e)}
+                    >
+                        <div className="serverModalInputContainer">
+                            <label htmlFor="name">Server Name</label>
+                            <input
+                                type="text"
+                                name="name"
+                                placeholder="Name"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                            ></input>
+                        </div>
+                        <div className="serverModalInputContainer">
+                            <label htmlFor="description">Description</label>
+                            <input
+                                type="text"
+                                name="description"
+                                placeholder="Description"
+                                value={description}
+                                onChange={(e) => setDescription(e.target.value)}
+                            ></input>
+                        </div>
+                        <div className="serverModalInputContainer">
+                            <label htmlFor="public">Privacy</label>
+                            <select
+                                onChange={(e) => setIsPublic(e.target.value)}
+                            >
+                                <option value="Public">Public</option>
+                                <option value="Private">Private</option>
+                            </select>
+                        </div>
+                        <div className="serverModalInputContainer">
+                            <label htmlFor="image">Image</label>
+                            <input
+                                type="text"
+                                name="image"
+                                placeholder="Image Url"
+                                value={image}
+                                onChange={(e) => setImage(e.target.value)}
+                            ></input>
+                        </div>
+                        <div className="serverModalButtonContainer">
+                            <button className="serverModalSubmit" type="submit">
+                                Create
+                            </button>
+                        </div>
+                    </form>
                 </div>
-                <form className="serverModalForm" onSubmit={(e) => onSubmit(e)}>
-                    <div className="serverModalInputContainer">
-                        <label htmlFor="name">Server Name</label>
-                        <input
-                            type="text"
-                            name="name"
-                            placeholder="Name"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                        ></input>
-                    </div>
-                    <div className="serverModalInputContainer">
-                        <label htmlFor="description">Description</label>
-                        <input
-                            type="text"
-                            name="description"
-                            placeholder="Description"
-                            value={description}
-                            onChange={(e) => setDescription(e.target.value)}
-                        ></input>
-                    </div>
-                    <div className="serverModalInputContainer">
-                        <label htmlFor="public">Privacy</label>
-                        <select onChange={(e) => setIsPublic(e.target.value)}>
-                            <option value="Public">Public</option>
-                            <option value="Private">Private</option>
-                        </select>
-                    </div>
-                    <div className="serverModalInputContainer">
-                        <label htmlFor="image">Image</label>
-                        <input
-                            type="text"
-                            name="image"
-                            placeholder="Image Url"
-                            value={image}
-                            onChange={(e) => setImage(e.target.value)}
-                        ></input>
-                    </div>
-                    <div className="serverModalButtonContainer">
-                        <button className="serverModalSubmit" type="submit">
-                            Create
-                        </button>
-                    </div>
-                </form>
-            </div>
+            </animated.div>
         </div>
     ) : null;
 }
