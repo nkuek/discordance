61f83f5eb44b9525e4b1344dceca03ce819427f1
nkuek <nkuek1@gmail.com>
2021-03-20 18:23:51 -0400

    navbar

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -1,80 +1,105 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Modal from 'react-modal';
 import LogoutButton from '../auth/LogoutButton/index';
-import LoginForm from "../auth/LoginForm/index";
-import SignUpForm from "../auth/SignUpForm/index";
+import LoginForm from '../auth/LoginForm/index';
+import SignUpForm from '../auth/SignUpForm/index';
 
 const customStyles = {
-  content : {
-    top                   : '50%',
-    left                  : '50%',
-    right                 : 'auto',
-    bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
-  }
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+    },
 };
 
 const NavBar = ({ authenticated, setAuthenticated }) => {
+    var subtitle;
+    const [modalIsOpenLogin, setIsOpenLogin] = useState(false);
+    const [modalIsOpenSignUp, setIsOpenSignUp] = useState(false);
+    function openModalLogin() {
+        setIsOpenLogin(true);
+    }
 
-var subtitle;
-  const [modalIsOpenLogin,setIsOpenLogin] = React.useState(false);
-  const [modalIsOpenSignUp,setIsOpenSignUp] = React.useState(false);
-  function openModalLogin() {
-    setIsOpenLogin(true);
-  }
+    function openModalSignUp() {
+        setIsOpenSignUp(true);
+    }
 
-  function openModalSignUp() {
-    setIsOpenSignUp(true);
-  }
+    function afterOpenModal() {
+        // references are now sync'd and can be accessed.
+        // subtitle.style.color = '#f00';
+    }
 
-  function afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    // subtitle.style.color = '#f00';
-  }
+    function closeModalLogin() {
+        setIsOpenLogin(false);
+    }
 
-  function closeModalLogin(){
-    setIsOpenLogin(false);
-  }
+    function closeModalSignUp() {
+        setIsOpenSignUp(false);
+    }
 
-  function closeModalSignUp(){
-    setIsOpenSignUp(false);
-  }
-
-  return (
-    <nav>
-      <div>
-          <div>
-            {authenticated === true ? '' : <button onClick={openModalLogin}>Login</button>}
-            <Modal
-              isOpen={authenticated === true ? false : modalIsOpenLogin}
-              onAfterOpen={afterOpenModal}
-              onRequestClose={closeModalLogin}
-              style={customStyles}
-              contentLabel="Example Modal"
-            >
-              <LoginForm authenticated={authenticated} setAuthenticated={setAuthenticated}/>
-            </Modal>
-          </div>
-          <div>
-            {authenticated === true ? '' : <button onClick={openModalSignUp}>Sign Up</button>}
-            <Modal
-              isOpen={authenticated === true ? false : modalIsOpenSignUp}
-              onAfterOpen={afterOpenModal}
-              onRequestClose={closeModalSignUp}
-              style={customStyles}
-              contentLabel="Example Modal"
-            >
-              <SignUpForm authenticated={authenticated} setAuthenticated={setAuthenticated}/>
-            </Modal>
-          </div>
-          <div>
-            {authenticated === false ? '' : <LogoutButton setAuthenticated={setAuthenticated} />}
-          </div>
-      </div>
-    </nav>
-
-  );
-}
+    return (
+        <nav>
+            <div>
+                <div>
+                    {authenticated === true ? (
+                        ''
+                    ) : (
+                        <button onClick={openModalLogin}>Login</button>
+                    )}
+                    <Modal
+                        isOpen={
+                            authenticated === true ? false : modalIsOpenLogin
+                        }
+                        onAfterOpen={afterOpenModal}
+                        onRequestClose={closeModalLogin}
+                        style={customStyles}
+                        contentLabel="Example Modal"
+                    >
+                        <LoginForm
+                            authenticated={authenticated}
+                            setAuthenticated={setAuthenticated}
+                        />
+                    </Modal>
+                </div>
+                <div>
+                    {authenticated === true ? (
+                        ''
+                    ) : (
+                        <button onClick={openModalSignUp}>Sign Up</button>
+                    )}
+                    <Modal
+                        isOpen={
+                            authenticated === true ? false : modalIsOpenSignUp
+                        }
+                        onAfterOpen={afterOpenModal}
+                        onRequestClose={closeModalSignUp}
+                        style={customStyles}
+                        contentLabel="Example Modal"
+                    >
+                        <SignUpForm
+                            authenticated={authenticated}
+                            setAuthenticated={setAuthenticated}
+                        />
+                    </Modal>
+                </div>
+                <button onClick={openServerModal}>Create a Server</button>
+                <ServerForm
+                    showServerModal={showServerModal}
+                    setShowServerModal={setShowServerModal}
+                />
+                <div>
+                    {authenticated === false ? (
+                        ''
+                    ) : (
+                        <LogoutButton setAuthenticated={setAuthenticated} />
+                    )}
+                </div>
+            </div>
+        </nav>
+    );
+};
 
 export default NavBar;
