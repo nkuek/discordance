88b0059db4d6ad20b46de4cbcf513579f984c1fa
nkuek <nkuek1@gmail.com>
2021-03-20 18:19:35 -0400

    fixed merge conflicts

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,95 +1,95 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-<<<<<<< HEAD
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 // import LoginForm from "./components/auth/LoginForm/index";
 // import SignUpForm from "./components/auth/SignUpForm/index";
-import NavBar from "./components/NavBar/index";
-import ProtectedRoute from "./components/auth/ProtectedRoute/index";
-import UsersList from "./components/UsersList";
-import User from "./components/User";
-import { authenticate } from "./store/auth";
-=======
-import LoginForm from "./components/auth/LoginForm";
-import LoginFormModal from "./components/LoginFormModal";
-import SignUpForm from "./components/auth/SignUpForm";
-import NavBar from "./components/NavBar";
-import ProtectedRoute from "./components/auth/ProtectedRoute";
-import UsersList from "./components/UsersList";
-import User from "./components/User";
-import { authenticate } from "./services/auth";
-import ServerForm from "./components/ServerForm";
-import Server from "./components/Server";
-<<<<<<< HEAD
->>>>>>> main
-=======
->>>>>>> main
+import NavBar from './components/NavBar/index';
+import ProtectedRoute from './components/auth/ProtectedRoute/index';
+import UsersList from './components/UsersList';
+import User from './components/User';
+import { authenticate } from './store/auth';
+import LoginForm from './components/auth/LoginForm';
+import LoginFormModal from './components/LoginFormModal';
+import SignUpForm from './components/auth/SignUpForm';
+import ServerForm from './components/ServerForm';
+import Server from './components/Server';
 
 function App() {
-  const [authenticated, setAuthenticated] = useState(false);
-  const [loaded, setLoaded] = useState(false);
+    const [authenticated, setAuthenticated] = useState(false);
+    const [loaded, setLoaded] = useState(false);
 
-  useEffect(() => {
-    (async () => {
-      const user = await authenticate();
-      if (!user.errors) {
-        setAuthenticated(true);
-      }
-      setLoaded(true);
-    })();
-  }, []);
+    useEffect(() => {
+        (async () => {
+            const user = await authenticate();
+            if (!user.errors) {
+                setAuthenticated(true);
+            }
+            setLoaded(true);
+        })();
+    }, []);
 
-  if (!loaded) {
-    return null;
-  }
+    if (!loaded) {
+        return null;
+    }
 
-  return (
-    <BrowserRouter>
-      <NavBar authenticated={authenticated} setAuthenticated={setAuthenticated} />
-      <Switch>
-        <ProtectedRoute path="/users" exact={true} authenticated={authenticated}>
-          <UsersList/>
-        </ProtectedRoute>
-        <Route path="/logins" exact={true}>
-          <LoginFormModal />
-        </Route>
-        <Route path="/login" exact={true}>
-          <LoginForm
-            authenticated={authenticated}
-            setAuthenticated={setAuthenticated}
-          />
-        </Route>
-        <Route path="/sign-up" exact={true}>
-          <SignUpForm
-            authenticated={authenticated}
-            setAuthenticated={setAuthenticated}
-          />
-        </Route>
-        <ProtectedRoute
-          path="/users"
-          exact={true}
-          authenticated={authenticated}
-        >
-          <UsersList />
-        </ProtectedRoute>
-        <ProtectedRoute
-          path="/users/:userId"
-          exact={true}
-          authenticated={authenticated}
-        >
-          <User />
-        </ProtectedRoute>
-        <ProtectedRoute path="/" exact={true} authenticated={authenticated}>
-          <h1>My Home Page</h1>
-        </ProtectedRoute>
-        <Route exact path="/servers/new">
-          <ServerForm />
-        </Route>
-        <Route path="/servers/:serverId">
-          <Server />
-        </Route>
-      </Switch>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <NavBar
+                authenticated={authenticated}
+                setAuthenticated={setAuthenticated}
+            />
+            <Switch>
+                <ProtectedRoute
+                    path="/users"
+                    exact={true}
+                    authenticated={authenticated}
+                >
+                    <UsersList />
+                </ProtectedRoute>
+                <Route path="/logins" exact={true}>
+                    <LoginFormModal />
+                </Route>
+                <Route path="/login" exact={true}>
+                    <LoginForm
+                        authenticated={authenticated}
+                        setAuthenticated={setAuthenticated}
+                    />
+                </Route>
+                <Route path="/sign-up" exact={true}>
+                    <SignUpForm
+                        authenticated={authenticated}
+                        setAuthenticated={setAuthenticated}
+                    />
+                </Route>
+                <ProtectedRoute
+                    path="/users"
+                    exact={true}
+                    authenticated={authenticated}
+                >
+                    <UsersList />
+                </ProtectedRoute>
+                <ProtectedRoute
+                    path="/users/:userId"
+                    exact={true}
+                    authenticated={authenticated}
+                >
+                    <User />
+                </ProtectedRoute>
+                <ProtectedRoute
+                    path="/"
+                    exact={true}
+                    authenticated={authenticated}
+                >
+                    <h1>My Home Page</h1>
+                </ProtectedRoute>
+                <Route exact path="/servers/new">
+                    <ServerForm />
+                </Route>
+                <Route path="/servers/:serverId">
+                    <Server />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;
