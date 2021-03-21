bb34a44d3e092355b6f61684fbf34dd92121e3ae
Yassine Cherradi <yassine.cherradi@yahoo.com>
2021-03-21 12:42:42 +0100

    styled and added cross login/signup links

--- react-app/src/components/auth/LoginForm/index.js
+++ react-app/src/components/auth/LoginForm/index.js
@@ -1,11 +1,18 @@
 import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { login } from '../../../store/auth';
+import './LoginForm.css';
 
-const LoginForm = ({ authenticated, setAuthenticated, setIsOpenLogin }) => {
+const LoginForm = ({ authenticated, setAuthenticated, setIsOpenLogin, openModalSignUp, closeModalLogin}) => {
     const [errors, setErrors] = useState([]);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    const onSignUp = (e) => {
+        e.preventDefault();
+        closeModalLogin();
+        openModalSignUp();
+    }
 
     const onLogin = async (e) => {
         e.preventDefault();
@@ -31,36 +38,52 @@ const LoginForm = ({ authenticated, setAuthenticated, setIsOpenLogin }) => {
     if (authenticated) {
         return <Redirect to="/" />;
     }
-
+    
     return (
-        <form onSubmit={onLogin}>
-            <div>
-                {errors.map((error) => (
-                    <div>{error}</div>
-                ))}
+        <div
+            className="LoginModalWrapper"
+        >
+            <div className="LoginModalContainer">
+                <div className="LoginModalFormTitleContainer">
+                    <div className="LoginModalFormTitle">
+                        Welcome Back
+                    </div>
+                </div>
+                <form onSubmit={onLogin}>
+                    <div className="LoginErrorModalContainer">
+                        {errors.map((error) => (
+                            <div>{error}</div>
+                        ))}
+                    </div>
+                    <div className="LoginModalInputContainer">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            name="email"
+                            type="text"
+                            placeholder="Email"
+                            value={email}
+                            onChange={updateEmail}
+                        />
+                    </div>
+                    <div className="LoginModalInputContainer">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            name="password"
+                            type="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={updatePassword}
+                        />
+                    </div>
+                    <div className="LoginModalButtonContainer">
+                        <button className="LoginModalSubmit" type="submit">Login</button>
+                    </div>
+                    <div>
+                        <button className="not-a-member" onClick={onSignUp}>Not a Member? Sign Up</button>
+                    </div>
+                </form>
             </div>
-            <div>
-                <label htmlFor="email">Email</label>
-                <input
-                    name="email"
-                    type="text"
-                    placeholder="Email"
-                    value={email}
-                    onChange={updateEmail}
-                />
-            </div>
-            <div>
-                <label htmlFor="password">Password</label>
-                <input
-                    name="password"
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={updatePassword}
-                />
-                <button type="submit">Login</button>
-            </div>
-        </form>
+        </div>    
     );
 };
 
--- react-app/src/components/auth/SignUpForm/index.js
+++ react-app/src/components/auth/SignUpForm/index.js
@@ -1,12 +1,19 @@
 import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { signUp } from '../../../store/auth';
+import './SignUpForm.css';
 
-const SignUpForm = ({ authenticated, setAuthenticated }) => {
+const SignUpForm = ({ authenticated, setAuthenticated, closeModalSignUp, openModalLogin }) => {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [repeatPassword, setRepeatPassword] = useState('');
+
+    const onLogin = (e) => {
+        e.preventDefault();
+        closeModalSignUp();
+        openModalLogin();
+    }
 
     const onSignUp = async (e) => {
         e.preventDefault();
@@ -40,46 +47,62 @@ const SignUpForm = ({ authenticated, setAuthenticated }) => {
     }
 
     return (
-        <form onSubmit={onSignUp}>
-            <div>
-                <label>User Name</label>
-                <input
-                    type="text"
-                    name="username"
-                    onChange={updateUsername}
-                    value={username}
-                ></input>
+        <div
+            className="SignUpModalWrapper"
+        >
+            <div className="SignUpModalContainer">
+                <div className="SignUpModalFormTitleContainer">
+                    <div className="SignUpModalFormTitle">
+                        Register
+                    </div>
+                </div>
+                <form onSubmit={onSignUp}>
+                    <div className="SignUpModalInputContainer">
+                        <label>User Name</label>
+                        <input
+                            type="text"
+                            name="username"
+                            onChange={updateUsername}
+                            value={username}
+                        ></input>
+                    </div>
+                    <div className="SignUpModalInputContainer">
+                        <label>Email</label>
+                        <input
+                            type="text"
+                            name="email"
+                            onChange={updateEmail}
+                            value={email}
+                        ></input>
+                    </div>
+                    <div className="SignUpModalInputContainer">
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            onChange={updatePassword}
+                            value={password}
+                        ></input>
+                    </div>
+                    <div className="SignUpModalInputContainer">
+                        <label>Repeat Password</label>
+                        <input
+                            type="password"
+                            name="repeat_password"
+                            onChange={updateRepeatPassword}
+                            value={repeatPassword}
+                            required={true}
+                        ></input>
+                    </div>
+                    <div className="SignUpModalButtonContainer">
+                        <button className="SignUpModalSubmit" type="submit">Sign Up</button>
+                    </div>
+                    <div>
+                        <button className="already-a-member" onClick={onLogin}>Already a Member? Log In</button>
+                    </div>
+                </form>
             </div>
-            <div>
-                <label>Email</label>
-                <input
-                    type="text"
-                    name="email"
-                    onChange={updateEmail}
-                    value={email}
-                ></input>
-            </div>
-            <div>
-                <label>Password</label>
-                <input
-                    type="password"
-                    name="password"
-                    onChange={updatePassword}
-                    value={password}
-                ></input>
-            </div>
-            <div>
-                <label>Repeat Password</label>
-                <input
-                    type="password"
-                    name="repeat_password"
-                    onChange={updateRepeatPassword}
-                    value={repeatPassword}
-                    required={true}
-                ></input>
-            </div>
-            <button type="submit">Sign Up</button>
-        </form>
+        </div>      
     );
 };
 
