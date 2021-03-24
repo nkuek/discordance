3253e33eff6997a50ad6087929230e294de09a76
nkuek <nkuek1@gmail.com>
2021-03-24 19:09:21 -0400

    deleting servers with messages works now

--- app/models/channel.py
+++ app/models/channel.py
@@ -18,7 +18,7 @@ class Channel(db.Model):
     )
 
     servers = db.relationship('Server', back_populates='channels')
-    messages = db.relationship('Message', back_populates='channel')
+    messages = db.relationship('Message', back_populates='channel', cascade='all, delete-orphan')
 
     def to_dict(self):
         return {
