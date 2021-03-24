e51d4bec307bdfb27d6d44c85beb895d6662b50f
Yassine Cherradi <yassine.cherradi@yahoo.com>
2021-03-24 22:30:26 +0100

    fixed bugs

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -12,7 +12,7 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     seed_users()
-    # seed_servers()
+    seed_servers()
     seed_images()
     # Add other seed functions here
 
