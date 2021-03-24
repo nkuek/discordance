f621b1656de16c4f7bda0501418707cae46fd663
nkuek <nkuek1@gmail.com>
2021-03-23 23:45:57 -0400

    fixed merge error

--- react-app/src/components/EditChannelForm/index.js
+++ react-app/src/components/EditChannelForm/index.js
@@ -1,17 +1,16 @@
-import React, { useState, useRef, useEffect, useCallback } from "react";
-import { useSpring, animated } from "react-spring";
-import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router-dom";
-import { updateExistingChannel } from "../../store/channel";
+import React, { useState, useRef, useEffect, useCallback } from 'react';
+import { useSpring, animated } from 'react-spring';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { updateExistingChannel } from '../../store/channel';
 
 function EditChannelForm({ showEditChannelModal, setShowEditChannelModal }) {
-  const dispatch = useDispatch();
-  const history = useHistory();
-  const [channelName, setChannelName] = useState("");
-  const [errors, setErrors] = useState("");
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const [channelName, setChannelName] = useState('');
+    const [errors, setErrors] = useState('');
 
-  const channel = useSelector((state) => state.channel);
-
+    const channel = useSelector((state) => state.channel);
 
     const editChannelModalRef = useRef();
     // close modal when clicking anywhere else
@@ -22,52 +21,50 @@ function EditChannelForm({ showEditChannelModal, setShowEditChannelModal }) {
         }
     };
 
-  useEffect(() => {
-    setChannelName(channel.name);
-  }, [channel]);
-
-  // close modal when pressing escape key
-  const keyPress = useCallback(
-    (e) => {
-      if (e.key === "Escape" && showEditChannelModal) {
-        setShowEditChannelModal(false);
+    useEffect(() => {
         setChannelName(channel.name);
-      }
-    },
-    [showEditChannelModal, setShowEditChannelModal]
-  );
+    }, [channel]);
 
-  useEffect(() => {
-    document.addEventListener("keydown", keyPress);
-    return () => document.removeEventListener("keydown", keyPress);
-  }, [keyPress]);
+    // close modal when pressing escape key
+    const keyPress = useCallback(
+        (e) => {
+            if (e.key === 'Escape' && showEditChannelModal) {
+                setShowEditChannelModal(false);
+                setChannelName(channel.name);
+            }
+        },
+        [showEditChannelModal, setShowEditChannelModal]
+    );
 
-  // Modal animations from react-spring
-  const animation = useSpring({
-    config: {
-      duration: 150,
-    },
-    opacity: showEditChannelModal ? 1 : 0,
-    transform: showEditChannelModal ? `scale(1)` : `scale(0.8)`,
-  });
+    useEffect(() => {
+        document.addEventListener('keydown', keyPress);
+        return () => document.removeEventListener('keydown', keyPress);
+    }, [keyPress]);
 
+    // Modal animations from react-spring
+    const animation = useSpring({
+        config: {
+            duration: 150,
+        },
+        opacity: showEditChannelModal ? 1 : 0,
+        transform: showEditChannelModal ? `scale(1)` : `scale(0.8)`,
+    });
 
     const handleEditChannel = async (e, updatedName, channelId) => {
         e.preventDefault();
 
-    if (!channelName) {
-      setErrors("Channel name cannot be empty!");
-      return;
-    }
+        if (!channelName) {
+            setErrors('Channel name cannot be empty!');
+            return;
+        }
 
-    setErrors("");
-    setChannelName("");
-    setShowEditChannelModal(false);
+        setErrors('');
+        setChannelName('');
+        setShowEditChannelModal(false);
 
-    dispatch(updateExistingChannel({ updatedName, channelId }));
-    // history.push(`/servers/${server.id}/${newChannel.id}`);
-  };
-
+        dispatch(updateExistingChannel({ updatedName, channelId }));
+        // history.push(`/servers/${server.id}/${newChannel.id}`);
+    };
 
     return showEditChannelModal ? (
         <div
@@ -113,9 +110,7 @@ function EditChannelForm({ showEditChannelModal, setShowEditChannelModal }) {
                 </div>
             </animated.div>
         </div>
-      </animated.div>
-    </div>
-  ) : null;
+    ) : null;
 }
 
 export default EditChannelForm;
