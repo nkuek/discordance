2b98a54ddeaf034eb768e021d9dc58de4d1abe62
Hussein Eid <husseineid@mochamacbook.myfiosgateway.com>
2021-03-23 23:27:24 -0400

    delete

--- react-app/src/components/Server/Sidebar/SidebarChannel.js
+++ react-app/src/components/Server/Sidebar/SidebarChannel.js
@@ -1,47 +1,43 @@
-import React, { useEffect, useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { findExistingServer } from '../../../store/server';
-import './SidebarChannel.css';
-import SettingsIcon from '@material-ui/icons/Settings';
-import { IconButton } from '@material-ui/core';
-import { withStyles } from '@material-ui/styles';
-import EditDeleteDropDown from '../../EditDeleteDropdown';
+import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { findExistingServer } from "../../../store/server";
+import "./SidebarChannel.css";
+import SettingsIcon from "@material-ui/icons/Settings";
+import { IconButton } from "@material-ui/core";
+import { withStyles } from "@material-ui/styles";
+import EditDeleteDropDown from "../../EditDeleteDropdown";
 
 function SidebarChannel() {
-    const dispatch = useDispatch();
-    const [isLoaded, setIsLoaded] = useState(false);
-    const server = useSelector((state) => state.server);
-    const channel = useSelector((state) => state.channel);
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+  const server = useSelector((state) => state.server);
+  const channel = useSelector((state) => state.channel);
 
-    useEffect(() => {
-        if (Object.keys(server).length > 0)
-            dispatch(findExistingServer(server?.id));
-    }, [dispatch, channel]);
+  useEffect(() => {
+    if (Object.keys(server).length > 0)
+      dispatch(findExistingServer(server?.id));
+  }, [dispatch, channel]);
 
-    useEffect(() => {
-        if (Object.keys(server).length > 0) setIsLoaded(true);
-    }, [server]);
+  useEffect(() => {
+    if (Object.keys(server).length > 0) setIsLoaded(true);
+  }, [server]);
 
-    return isLoaded ? (
-        <>
-            {server?.channels.map((channel) => (
-                <div key={channel.id} className="sidebarChannel">
-                    <NavLink
-                        className="sidebarChannelLink"
-                        to={`/servers/${server.id}/${channel.id}`}
-                    >
-                        <div className="channelNameAndHash">
-                            <span className="sidebarChannel__hash">#</span>
-                            <div className="channelName">{channel.name}</div>
-                        </div>
-
-                        <EditDeleteDropDown />
-                    </NavLink>
-                </div>
-            ))}
-        </>
-    ) : null;
+  return isLoaded ? (
+    <div className="sidebarChannelLink">
+      {server?.channels.map((channel) => (
+        <div key={channel.id} className="sidebarChannel">
+          <NavLink to={`/servers/${server.id}/${channel.id}`}>
+            <div className="channelNameAndHash">
+              <span className="sidebarChannel__hash">#</span>
+              <div className="channelName">{channel.name}</div>
+            </div>
+          </NavLink>
+          <EditDeleteDropDown />
+        </div>
+      ))}
+    </div>
+  ) : null;
 }
 
 export default SidebarChannel;
--- react-app/src/store/channel.js
+++ react-app/src/store/channel.js
@@ -1,77 +1,98 @@
-const ADD_CHANNEL = 'channel/addChannel';
-const GET_CHANNEL = 'channel/getChannel';
-const EDIT_CHANNEL = 'channel/editChannel';
+const ADD_CHANNEL = "channel/addChannel";
+const GET_CHANNEL = "channel/getChannel";
+const EDIT_CHANNEL = "channel/editChannel";
 
 const addChannel = (newChannel) => ({
-    type: ADD_CHANNEL,
-    newChannel,
+  type: ADD_CHANNEL,
+  newChannel,
 });
 
 const getChannel = (channel) => ({
-    type: GET_CHANNEL,
-    channel,
+  type: GET_CHANNEL,
+  channel,
 });
 
 const editChannel = (updatedChannel) => ({
-    type: EDIT_CHANNEL,
-    updatedChannel,
+  type: EDIT_CHANNEL,
+  updatedChannel,
 });
 
 //add a channel
 export const createChannel = (name, serverId) => async (dispatch) => {
-    const response = await fetch('/api/servers/:id/:channel_id/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            name,
-            serverId,
-        }),
-    });
-    const data = await response.json();
-    dispatch(addChannel(data));
-    return data;
+  const response = await fetch("/api/servers/:id/:channel_id/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name,
+      serverId,
+    }),
+  });
+  const data = await response.json();
+  dispatch(addChannel(data));
+  return data;
 };
 
 export const findExistingChannel = (channelId) => async (dispatch) => {
-    const response = await fetch('/api/servers/:id/:channel_id/', {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(channelId),
-    });
-    const data = await response.json();
-    dispatch(getChannel(data));
-    return data;
+  const response = await fetch("/api/servers/:id/:channel_id/", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(channelId),
+  });
+  const data = await response.json();
+  dispatch(getChannel(data));
+  return data;
 };
 
 export const updateExistingChannel = (updatedChannel) => async (dispatch) => {
-    const response = await fetch('/api/server/:id/:channel_id/edit/', {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(updatedChannel),
-    });
-    const data = await response.json();
-    dispatch(editChannel(data));
-    return data;
+  const response = await fetch("/api/servers/:id/:channel_id/edit/", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(updatedChannel),
+  });
+  const data = await response.json();
+  dispatch(editChannel(data));
+  return data;
+};
+
+//delete existing channel
+const DELETE_CHANNEL = "channel/deleteChannel";
+
+const deleteChannel = () => ({
+  type: DELETE_CHANNEL,
+});
+
+export const deleteExistingChannel = (channelId) => async (dispatch) => {
+  await fetch("/api/servers/:id/:channel_id", {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(channelId),
+  });
+  dispatch(deleteChannel());
 };
 
 const initialState = {};
 const channelReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case ADD_CHANNEL:
-            return action.newChannel;
-        case GET_CHANNEL:
-            return action.channel;
-        case EDIT_CHANNEL:
-            return action.updatedChannel;
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case ADD_CHANNEL:
+      return action.newChannel;
+    case GET_CHANNEL:
+      return action.channel;
+    case EDIT_CHANNEL:
+      return action.updatedChannel;
+    case DELETE_CHANNEL:
+      state = {};
+      return state;
+    default:
+      return state;
+  }
 };
 
 export default channelReducer;
