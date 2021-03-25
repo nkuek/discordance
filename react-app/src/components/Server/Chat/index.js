dbb2a45bc04674cfa8083ffe2ed6bcfdcb96b143
nkuek <nkuek1@gmail.com>
2021-03-25 18:09:20 -0400

    message id saved to state

--- react-app/src/components/Server/Chat/index.js
+++ react-app/src/components/Server/Chat/index.js
@@ -1,121 +1,134 @@
-import React, { Children, useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useParams, useHistory } from "react-router-dom";
-import { findExistingChannel } from "../../../store/channel";
-import "./Chat.css";
-import ChatHeader from "./ChatHeader";
-import AddCircleIcon from "@material-ui/icons/AddCircle";
-import CardGiftcardIcon from "@material-ui/icons/CardGiftcard";
-import { Avatar } from "@material-ui/core";
-import { IconButton } from "@material-ui/core";
-import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
-import GifIcon from "@material-ui/icons/Gif";
-import EmojiEmotionsIcon from "@material-ui/icons/EmojiEmotions";
-import io from "socket.io-client";
-import createNewMessage from "../../../store/chat";
-import MessageDropdown from "../../MessageDropdown";
+import React, { Children, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams, useHistory } from 'react-router-dom';
+import { findExistingChannel } from '../../../store/channel';
+import './Chat.css';
+import ChatHeader from './ChatHeader';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import CardGiftcardIcon from '@material-ui/icons/CardGiftcard';
+import { Avatar } from '@material-ui/core';
+import { IconButton } from '@material-ui/core';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import GifIcon from '@material-ui/icons/Gif';
+import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
+import io from 'socket.io-client';
+import createNewMessage from '../../../store/chat';
+import MessageDropdown from '../../MessageDropdown';
+import { saveMessageToState } from '../../../store/message';
 
-const socket = io("http://localhost:5000/");
+const socket = io('http://localhost:5000/');
 
 function Chat() {
-  const chatBox = document.querySelector(".chat__messages");
-  const dispatch = useDispatch();
-  const [messageInput, setMessageInput] = useState("");
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [newMessage, setNewMessage] = useState(false);
+    const chatBox = document.querySelector('.chat__messages');
+    const dispatch = useDispatch();
+    const [messageInput, setMessageInput] = useState('');
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [newMessage, setNewMessage] = useState(false);
 
-  const { channelId } = useParams();
+    const { channelId } = useParams();
 
-  const user = useSelector((state) => state.session.user);
-  const channel = useSelector((state) => state.channel);
+    const user = useSelector((state) => state.session.user);
+    const channel = useSelector((state) => state.channel);
 
-  const handleNewMessage = (e) => {
-    e.preventDefault();
-    if (!messageInput) return;
-    setMessageInput("");
-    socket.emit("new message");
-    createNewMessage(messageInput, user, channel);
-    setNewMessage(true);
-  };
+    const handleNewMessage = (e) => {
+        e.preventDefault();
+        if (!messageInput) return;
+        setMessageInput('');
+        socket.emit('new message');
+        createNewMessage(messageInput, user, channel);
+        setNewMessage(true);
+    };
 
-  useEffect(() => {
-    socket.on("load message", () => {
-      setNewMessage(true);
-    });
-  }, []);
+    const handleDropdown = (messageId) => {
+        dispatch(saveMessageToState(messageId));
+    };
 
-  useEffect(() => {
-    console.log("dispatching");
-    dispatch(findExistingChannel(channelId));
-    setNewMessage(false);
-  }, [channelId, newMessage]);
+    useEffect(() => {
+        socket.on('load message', () => {
+            setNewMessage(true);
+        });
+    }, []);
 
-  useEffect(() => {
-    if (channel) {
-      setIsLoaded(true);
-      if (chatBox) chatBox.scrollTop = chatBox.scrollHeight;
-    }
-  }, [channel]);
+    useEffect(() => {
+        console.log('dispatching');
+        dispatch(findExistingChannel(channelId));
+        setNewMessage(false);
+    }, [channelId, newMessage]);
 
-  return (
-    isLoaded && (
-      <div className="chat">
-        <ChatHeader />
+    useEffect(() => {
+        if (channel) {
+            setIsLoaded(true);
+            if (chatBox) chatBox.scrollTop = chatBox.scrollHeight;
+        }
+    }, [channel]);
 
-        <div className="chat__messages">
-          {channel.messages &&
-            channel.messages.length > 0 &&
-            channel.messages.map((message, idx) => (
-              <div key={idx} className="chatMessageContainer">
-                <div className="chatImageAndBody">
-                  <div className="chatImageAndName">
-                    {!user || user.profile_URL === undefined ? (
-                      <Avatar />
-                    ) : (
-                      <div>
-                        <img
-                          className="profile__image"
-                          src={`${user.profile_URL}`}
+    return (
+        isLoaded && (
+            <div className="chat">
+                <ChatHeader />
+
+                <div className="chat__messages">
+                    {channel.messages &&
+                        channel.messages.length > 0 &&
+                        channel.messages.map((message, idx) => (
+                            <div key={idx} className="chatMessageContainer">
+                                <div className="chatImageAndBody">
+                                    <div className="chatImageAndName">
+                                        {!user ||
+                                        user.profile_URL === undefined ? (
+                                            <Avatar />
+                                        ) : (
+                                            <div>
+                                                <img
+                                                    className="profile__image"
+                                                    src={`${user.profile_URL}`}
+                                                />
+                                            </div>
+                                        )}
+                                    </div>
+                                    <div className="messageBodyAndButtons">
+                                        <div className="messageBody">
+                                            <p className="chatUsername">
+                                                {message.username}
+                                            </p>
+                                            <p className="chatMessage">
+                                                {message.message}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div
+                                    onClick={() => handleDropdown(message.id)}
+                                    className="messageButtons"
+                                >
+                                    <MessageDropdown />
+                                </div>
+                            </div>
+                        ))}
+                </div>
+
+                <div className="chat__input">
+                    <AddCircleIcon fontSize="large" />
+                    <form onSubmit={(e) => handleNewMessage(e)}>
+                        <input
+                            value={messageInput}
+                            onChange={(e) => setMessageInput(e.target.value)}
+                            placeholder={`message #TEST`}
                         />
-                      </div>
-                    )}
-                  </div>
-                  <div className="messageBodyAndButtons">
-                    <div className="messageBody">
-                      <p className="chatUsername">{message.username}</p>
-                      <p className="chatMessage">{message.message}</p>
+                        <button className="chat__inputButton" type="submit">
+                            Send Message
+                        </button>
+                    </form>
+
+                    <div className="chat__inputIcons">
+                        <CardGiftcardIcon fontSize="large" />
+                        <GifIcon fontSize="large" />
+                        <EmojiEmotionsIcon fontSize="large" />
                     </div>
-                  </div>
                 </div>
-                <div className="messageButtons">
-                  <MessageDropdown />
-                </div>
-              </div>
-            ))}
-        </div>
-
-        <div className="chat__input">
-          <AddCircleIcon fontSize="large" />
-          <form onSubmit={(e) => handleNewMessage(e)}>
-            <input
-              value={messageInput}
-              onChange={(e) => setMessageInput(e.target.value)}
-              placeholder={`message #TEST`}
-            />
-            <button className="chat__inputButton" type="submit">
-              Send Message
-            </button>
-          </form>
-
-          <div className="chat__inputIcons">
-            <CardGiftcardIcon fontSize="large" />
-            <GifIcon fontSize="large" />
-            <EmojiEmotionsIcon fontSize="large" />
-          </div>
-        </div>
-      </div>
-    )
-  );
+            </div>
+        )
+    );
 }
 
 export default Chat;
--- react-app/src/store/message.js
+++ react-app/src/store/message.js
@@ -1,31 +1,43 @@
-const DELETE_MESSAGE = "message/deleteMessage";
+const DELETE_MESSAGE = 'message/deleteMessage';
+const SAVE_MESSAGE = 'message/saveMessage';
 
 const deleteMessage = () => ({
-  type: DELETE_MESSAGE,
+    type: DELETE_MESSAGE,
+});
+
+const saveMessage = (messageId) => ({
+    type: SAVE_MESSAGE,
+    messageId,
 });
 
 // Delete existing message
 export const deleteExistingMessage = (messageId) => async (dispatch) => {
-  await fetch("/api/chat/delete/", {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(messageId),
-  });
-  console.log(messageId);
-  dispatch(deleteMessage());
+    await fetch('/api/chat/delete/', {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(messageId),
+    });
+    console.log(messageId);
+    dispatch(deleteMessage());
+};
+
+export const saveMessageToState = (messageId) => (dispatch) => {
+    dispatch(saveMessage(messageId));
 };
 
 const initialState = {};
 const messageReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case DELETE_MESSAGE:
-      state = {};
-      return state;
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case DELETE_MESSAGE:
+            state = {};
+            return state;
+        case SAVE_MESSAGE:
+            return action.messageId;
+        default:
+            return state;
+    }
 };
 
 export default messageReducer;
