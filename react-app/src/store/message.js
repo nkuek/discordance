a6c412757c7719546efdd9c7e9dcc1a68c71319f
Hussein Eid <husseineid@mochamacbook.myfiosgateway.com>
2021-03-26 00:15:42 -0400

    edit message store

--- react-app/src/store/message.js
+++ react-app/src/store/message.js
@@ -1,5 +1,6 @@
 const DELETE_MESSAGE = "message/deleteMessage";
 const SAVE_MESSAGE = "message/saveMessage";
+const EDIT_MESSAGE = "message/editMessage";
 
 const deleteMessage = () => ({
   type: DELETE_MESSAGE,
@@ -8,6 +9,11 @@ const deleteMessage = () => ({
 const saveMessage = (messageId) => ({
   type: SAVE_MESSAGE,
   messageId,
+});
+
+const editMessage = (updatedMessage) => ({
+  type: EDIT_MESSAGE,
+  updatedMessage,
 });
 
 // Delete existing message
@@ -26,12 +32,28 @@ export const saveMessageToState = (messageId) => (dispatch) => {
   dispatch(saveMessage(messageId));
 };
 
+// Edit existing message
+export const updateExistingMessage = (updatedMessage) => async (dispatch) => {
+  const response = await fetch("/api/chat/edit/", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(updatedMessage),
+  });
+  const data = await response.json();
+  dispatch(editMessage);
+  return data;
+};
+
 const initialState = {};
 const messageReducer = (state = initialState, action) => {
   switch (action.type) {
     case DELETE_MESSAGE:
       state = {};
       return state;
+    case EDIT_MESSAGE:
+      return action.updatedMessage;
     case SAVE_MESSAGE:
       return action.messageId;
     default:
