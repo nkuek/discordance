29bcbdeb5f783b565684c6a4bf4fd80afd7f2b3f
leoworkcp <leoworkcp@gmail.com>
2021-03-26 18:32:16 -0400

    validation back/front for email and username/ missing passwordgit add .!

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -5,15 +5,34 @@ from app.models import User
 
 
 def user_exists(form, field):
-    print("Checking if user exits", field.data)
+    print("Checking if user exists", field.data)
     email = field.data
     user = User.query.filter(User.email == email).first()
     if user:
         raise ValidationError("User is already registered.")
 
 
+def username_exists(form, field):
+    print("Checking if username exists",  field.data)
+    username = field.data
+    user = User.query.filter(User.username == username).first()
+    if user:
+        raise ValidationError("User Name is already registered.")
+
+
+# def password_matches(form, field):
+#     print("Checking if password matches", field.data)
+#     password = field.data
+#     repeat_password = field.data
+#     user = repeat_password.filter(repeat_password == password)
+#     if user:
+#         raise ValidationError("Password didn't match.")
+
+
 class SignUpForm(FlaskForm):
-    username = StringField('username', validators=[DataRequired()])
+    username = StringField('username', validators=[
+        DataRequired(), username_exists])
     email = StringField('email', validators=[DataRequired(), user_exists])
-    password = StringField('password', validators=[DataRequired()])
+    password = StringField('password', validators=[
+        DataRequired()])
     profile_URL = StringField('profile_URL')
---
+++ react-app/src/components/auth/SignUpForm/usePasswordValitation.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+export const usePasswordValidation = ({
+  firstPassword = "",
+  secondPassword = "",
+  requiredLength = 8,
+}) => {
+  const [validLength, setValidLength] = useState(null);
+  const [hasNumber, setHasNumber] = useState(null);
+  const [upperCase, setUpperCase] = useState(null);
+  const [lowerCase, setLowerCase] = useState(null);
+  const [specialChar, setSpecialChar] = useState(null);
+  const [match, setMatch] = useState(null);
+
+  useEffect(() => {
+    setValidLength(firstPassword.length >= requiredLength ? true : false);
+    setUpperCase(firstPassword.toLowerCase() !== firstPassword);
+    setLowerCase(firstPassword.toUpperCase() !== firstPassword);
+    setHasNumber(/\d/.test(firstPassword));
+    setMatch(firstPassword && firstPassword === secondPassword);
+    setSpecialChar(/[ `!@#$%^&*()_+\-=\]{};':"\\|,.<>?~]/.test(firstPassword));
+  }, [firstPassword, secondPassword, requiredLength]);
+
+  return [validLength, hasNumber, upperCase, lowerCase, match, specialChar];
+};
