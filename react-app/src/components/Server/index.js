fef70fe726df688eb1bc4656a927fb6bb7ff3451
nkuek <nkuek1@gmail.com>
2021-03-25 20:34:04 -0400

    fixed server infinite loop bug

--- react-app/src/components/Server/index.js
+++ react-app/src/components/Server/index.js
@@ -1,52 +1,52 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Route, useParams, useHistory } from "react-router-dom";
-import "./Server.css";
-import Sidebar from "./Sidebar";
-import Chat from "./Chat";
-import Message from "./Message";
-import { findExistingServer } from "../../store/server";
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Route, useParams, useHistory } from 'react-router-dom';
+import './Server.css';
+import Sidebar from './Sidebar';
+import Chat from './Chat';
+import Message from './Message';
+import { findExistingServer } from '../../store/server';
 
 function Server() {
-  const dispatch = useDispatch();
-  const history = useHistory();
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [firstLoadedRender, setFirstLoadedRender] = useState(false);
-  const { serverId } = useParams();
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [firstLoadedRender, setFirstLoadedRender] = useState(false);
+    const { serverId } = useParams();
 
-  const server = useSelector((state) => state.server);
+    const server = useSelector((state) => state.server);
 
-  useEffect(() => {
-    dispatch(findExistingServer(serverId));
-  }, [dispatch, serverId]);
+    useEffect(() => {
+        dispatch(findExistingServer(serverId));
+    }, [dispatch]);
 
-  useEffect(() => {
-    setIsLoaded(true);
-    setFirstLoadedRender(true);
-  }, [server]);
+    useEffect(() => {
+        setIsLoaded(true);
+        setFirstLoadedRender(true);
+    }, [server]);
 
-  useEffect(() => {
-    setFirstLoadedRender(false);
-  }, [serverId]);
+    useEffect(() => {
+        setFirstLoadedRender(false);
+    }, [serverId]);
 
-  useEffect(() => {
-    if (server.channels && server.channels.length > 0 && firstLoadedRender)
-      history.push(`/servers/${server.id}/${server.channels[0].id}`);
-  }, [firstLoadedRender, server.id, server.channels, history]);
+    useEffect(() => {
+        if (server.channels && server.channels.length > 0 && firstLoadedRender)
+            history.push(`/servers/${server.id}/${server.channels[0].id}`);
+    }, [firstLoadedRender, server.id, server.channels, history]);
 
-  return (
-    isLoaded && (
-      <>
-        <div className="serverContainer">
-          <Sidebar />
-          <Message />
-          <Route path={`/servers/:serverId/:channelId`}>
-            <Chat />
-          </Route>
-        </div>
-      </>
-    )
-  );
+    return (
+        isLoaded && (
+            <>
+                <div className="serverContainer">
+                    <Sidebar />
+                    <Message />
+                    <Route path={`/servers/:serverId/:channelId`}>
+                        <Chat />
+                    </Route>
+                </div>
+            </>
+        )
+    );
 }
 
 export default Server;
