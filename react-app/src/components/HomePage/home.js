cc4ddc1cb7948582c569749baafc4954d6ee905a
Yassine Cherradi <yassine.cherradi@yahoo.com>
2021-03-27 22:03:09 +0100

    fixed login modal rendering on home if not logged in

--- react-app/src/components/HomePage/home.js
+++ react-app/src/components/HomePage/home.js
@@ -1,14 +1,77 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 // import Discover from "./discover";
+import Modal from "react-modal";
+import LoginForm from "../auth/LoginForm/index";
+import SignUpForm from "../auth/SignUpForm/index";
+import Discover from './discover';
 
 import * as serverActions from '../../store/publicServer';
 import './Homepage.css';
 import SearchBar from '../SearchBar';
 
-function Home() {
+
+const customStyles = {
+    overlay: {
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: "rgba(0, 0, 0, 0.8)",
+    },
+    content: {
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        right: "auto",
+        bottom: "auto",
+        marginRight: "-50%",
+        transform: "translate(-50%, -50%)",
+        borderRadius: "10px",
+        padding: "20px",
+        backgroundColor: "#2c2f33",
+        borderColor: "#2f3135",
+    },
+};
+
+Modal.setAppElement("#root");
+
+
+function Home({authenticated, setAuthenticated}) {
     const dispatch = useDispatch();
+    const [modalIsOpenLogin, setIsOpenLogin] = useState(false);
+    const [modalIsOpenSignUp, setIsOpenSignUp] = useState(false);
+
+    //   const userServers = useSelector((state) => state?.userServers);
+    //   const loggedInUser = useSelector((state) => state?.session.user);
+    //   const server = useSelector((state) => state.server);
+
+    // debugger;
+
+    function openModalLogin() {
+        setIsOpenLogin(true);
+    }
+
+    function openModalSignUp() {
+        setIsOpenSignUp(true);
+    }
+
+    function afterOpenModal() {
+        // references are now sync'd and can be accessed.
+        // subtitle.style.color = '#f00';
+    }
+
+    function closeModalLogin() {
+        setIsOpenLogin(false);
+    }
+
+    function closeModalSignUp() {
+        setIsOpenSignUp(false);
+    }
+
+
     useEffect(() => {
         dispatch(serverActions.findPublicServers());
     }, [dispatch]);
@@ -17,7 +80,7 @@ function Home() {
     const publicServers = useSelector((state) => state?.publicServer);
 
     return (
-        <>
+        <>  
             {/* home page */}
             <div className="home-div__container">
                 <div className="banner-div__container">
@@ -26,12 +89,12 @@ function Home() {
                         alt="home-banner"
                     />
                     <div className="banner-text__container">
+                        <SearchBar />
                         <h1>Find your community on Discordance</h1>
                         <h2>
                             From gaming, to music, to learning, there's a place
                             for you.
                         </h2>
-                        <SearchBar />
                     </div>
                 </div>
 
@@ -40,7 +103,7 @@ function Home() {
                     <div className="servers-containers__home">
                         {Object.values(publicServers).map((el) => (
                             <div className="server-div__container" key={el?.id}>
-                                <NavLink to={`servers/${el?.id}`}>
+                                <NavLink onClick={openModalLogin} to={user ? `servers/${el?.id}` : '/'}>
                                     <div className="img-div__container">
                                         <img
                                             src={
@@ -62,6 +125,35 @@ function Home() {
                                 </NavLink>
                             </div>
                         ))}
+                        <Modal
+                        isOpen={modalIsOpenLogin}
+                        // onAfterOpen={afterOpenModal}
+                        onRequestClose={closeModalLogin}
+                        style={customStyles}
+                        contentLabel="Example Modal"
+                        >
+                        <LoginForm
+                            setIsOpenLogin={setIsOpenLogin}
+                            authenticated={authenticated}
+                            setAuthenticated={setAuthenticated}
+                            closeModalLogin={closeModalLogin}
+                            openModalSignUp={openModalSignUp}
+                        />
+                        </Modal>
+                        <Modal
+                        isOpen={modalIsOpenSignUp}
+                        // onAfterOpen={afterOpenModal}
+                        onRequestClose={closeModalSignUp}
+                        style={customStyles}
+                        contentLabel="Example Modal"
+                        >
+                        <SignUpForm
+                            authenticated={authenticated}
+                            setAuthenticated={setAuthenticated}
+                            closeModalSignUp={closeModalSignUp}
+                            openModalLogin={openModalLogin}
+                        />
+                        </Modal>
                     </div>
                 </div>
             </div>
