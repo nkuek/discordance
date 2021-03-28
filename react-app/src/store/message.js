5045f2c1f286273abf24997729bcb81cc5285cba
nkuek <nkuek1@gmail.com>
2021-03-28 00:35:08 -0400

    fixed chat likes

--- react-app/src/store/message.js
+++ react-app/src/store/message.js
@@ -1,86 +1,86 @@
-const DELETE_MESSAGE = "message/deleteMessage";
-const SAVE_MESSAGE = "message/saveMessage";
-const EDIT_MESSAGE = "message/editMessage";
-const ADD_LIKE = "message/addLike";
+const DELETE_MESSAGE = 'message/deleteMessage';
+const SAVE_MESSAGE = 'message/saveMessage';
+const EDIT_MESSAGE = 'message/editMessage';
+const ADD_LIKE = 'message/addLike';
 
 const deleteMessage = () => ({
-  type: DELETE_MESSAGE,
+    type: DELETE_MESSAGE,
 });
 
 const saveMessage = (message) => ({
-  type: SAVE_MESSAGE,
-  message,
+    type: SAVE_MESSAGE,
+    message,
 });
 
 const editMessage = (updatedMessage) => ({
-  type: EDIT_MESSAGE,
-  updatedMessage,
+    type: EDIT_MESSAGE,
+    updatedMessage,
 });
 
-const addLike = (newNumber) => ({
-  type: ADD_LIKE,
-  newNumber,
+const addLike = (updatedMessage) => ({
+    type: ADD_LIKE,
+    updatedMessage,
 });
 
 // Delete existing message
 export const deleteExistingMessage = (messageId) => async (dispatch) => {
-  await fetch("/api/chat/delete/", {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(messageId),
-  });
-  dispatch(deleteMessage());
+    await fetch('/api/chat/delete/', {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(messageId),
+    });
+    dispatch(deleteMessage());
 };
 
 export const saveMessageToState = (message) => (dispatch) => {
-  // console.log(message);
-  dispatch(saveMessage(message));
+    // console.log(message);
+    dispatch(saveMessage(message));
 };
 
 // Edit existing message
 export const updateExistingMessage = (updatedMessage) => async (dispatch) => {
-  const response = await fetch("/api/chat/edit/", {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(updatedMessage),
-  });
-  const data = await response.json();
-  dispatch(editMessage);
-  return data;
+    const response = await fetch('/api/chat/edit/', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updatedMessage),
+    });
+    const data = await response.json();
+    dispatch(editMessage);
+    return data;
 };
 
 export const addMessageLike = (messageId) => async (dispatch) => {
-  const response = await fetch("/api/chat/like/", {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({messageId}),
-  });
-  const data = await response.json();
-  dispatch(addLike);
-  return data;
+    const response = await fetch('/api/chat/like/', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ messageId }),
+    });
+    const updatedMessage = await response.json();
+    dispatch(addLike(updatedMessage));
+    return updatedMessage;
 };
 
 const initialState = {};
 const messageReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case DELETE_MESSAGE:
-      state = {};
-      return state;
-    case EDIT_MESSAGE:
-      return action.updatedMessage;
-    case SAVE_MESSAGE:
-      return action.message;
-    case ADD_LIKE:
-      return action.newNumber;
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case DELETE_MESSAGE:
+            state = {};
+            return state;
+        case EDIT_MESSAGE:
+            return action.updatedMessage;
+        case SAVE_MESSAGE:
+            return action.message;
+        case ADD_LIKE:
+            return action.updatedMessage;
+        default:
+            return state;
+    }
 };
 
 export default messageReducer;
