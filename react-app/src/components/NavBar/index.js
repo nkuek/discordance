bb71373b66be3459ad8dc144455ab9288e3427dc
nkuek <nkuek1@gmail.com>
2021-03-28 02:59:18 -0400

    added logo

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -1,63 +1,62 @@
-import React, { useState } from "react";
-import Modal from "react-modal";
-import LogoutButton from "../auth/LogoutButton/index";
-import LoginForm from "../auth/LoginForm/index";
-import SignUpForm from "../auth/SignUpForm/index";
-import "./NavBar.css";
-import { NavLink } from "react-router-dom";
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+import LogoutButton from '../auth/LogoutButton/index';
+import LoginForm from '../auth/LoginForm/index';
+import SignUpForm from '../auth/SignUpForm/index';
+import './NavBar.css';
+import { NavLink } from 'react-router-dom';
 
 const customStyles = {
-  overlay: {
-    position: "fixed",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: "rgba(0, 0, 0, 0.8)",
-    zIndex: 5,
-  },
-  content: {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
-    borderRadius: "10px",
-    padding: "20px",
-    backgroundColor: "#2c2f33",
-    border: "none",
-  },
+    overlay: {
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: 'rgba(0, 0, 0, 0.8)',
+        zIndex: 5,
+    },
+    content: {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+        borderRadius: '10px',
+        padding: '20px',
+        backgroundColor: '#2c2f33',
+        border: 'none',
+    },
 };
 
-Modal.setAppElement("#root");
+Modal.setAppElement('#root');
 
 const NavBar = ({ authenticated, setAuthenticated }) => {
-  const [modalIsOpenLogin, setIsOpenLogin] = useState(false);
-  const [modalIsOpenSignUp, setIsOpenSignUp] = useState(false);
+    const [modalIsOpenLogin, setIsOpenLogin] = useState(false);
+    const [modalIsOpenSignUp, setIsOpenSignUp] = useState(false);
 
-  function openModalLogin() {
-    setIsOpenLogin(true);
-  }
+    function openModalLogin() {
+        setIsOpenLogin(true);
+    }
 
-  function openModalSignUp() {
-    setIsOpenSignUp(true);
-  }
+    function openModalSignUp() {
+        setIsOpenSignUp(true);
+    }
 
-  function afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    // subtitle.style.color = '#f00';
-  }
+    function afterOpenModal() {
+        // references are now sync'd and can be accessed.
+        // subtitle.style.color = '#f00';
+    }
 
-  function closeModalLogin() {
-    setIsOpenLogin(false);
-  }
+    function closeModalLogin() {
+        setIsOpenLogin(false);
+    }
 
-  function closeModalSignUp() {
-    setIsOpenSignUp(false);
-  }
-
+    function closeModalSignUp() {
+        setIsOpenSignUp(false);
+    }
 
     return (
         <nav className="mainNavBar">
@@ -68,6 +67,10 @@ const NavBar = ({ authenticated, setAuthenticated }) => {
                             Developers
                         </NavLink>
                     </div>
+                </div>
+                <div className="mainLogoContainer">
+                    <div className="mainLogo">D3</div>
+                    <div className="mainLogoText">Discordance</div>
                 </div>
                 <div className="LoginSignupLogout">
                     <div>
