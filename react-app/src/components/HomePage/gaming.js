f66856c327b4017ebc299f983ada19ec9ac53193
nkuek <nkuek1@gmail.com>
2021-03-28 03:40:51 -0400

    server categories styling

--- react-app/src/components/HomePage/gaming.js
+++ react-app/src/components/HomePage/gaming.js
@@ -1,160 +1,175 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { NavLink } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 
-import Modal from "react-modal";
-import LoginForm from "../auth/LoginForm/index";
-import SignUpForm from "../auth/SignUpForm/index";
+import Modal from 'react-modal';
+import LoginForm from '../auth/LoginForm/index';
+import SignUpForm from '../auth/SignUpForm/index';
 
-import Discover from "./discover";
-import * as serverActions from "../../store/publicServer";
-import "./Homepage.css";
-import SearchBar from "../SearchBar";
+import Discover from './discover';
+import * as serverActions from '../../store/publicServer';
+import './Homepage.css';
+import SearchBar from '../SearchBar';
+import SportsEsportsIcon from '@material-ui/icons/SportsEsports';
 
 const customStyles = {
-  overlay: {
-    position: "fixed",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: "rgba(0, 0, 0, 0.8)",
-  },
-  content: {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
-    borderRadius: "10px",
-    padding: "20px",
-    backgroundColor: "#2c2f33",
-    borderColor: "#2f3135",
-  },
+    overlay: {
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: 'rgba(0, 0, 0, 0.8)',
+    },
+    content: {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+        borderRadius: '10px',
+        padding: '20px',
+        backgroundColor: '#2c2f33',
+        borderColor: '#2f3135',
+    },
 };
 
-Modal.setAppElement("#root");
+Modal.setAppElement('#root');
 
 function GamePage({ authenticated, setAuthenticated }) {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  const [modalIsOpenLogin, setIsOpenLogin] = useState(false);
-  const [modalIsOpenSignUp, setIsOpenSignUp] = useState(false);
+    const [modalIsOpenLogin, setIsOpenLogin] = useState(false);
+    const [modalIsOpenSignUp, setIsOpenSignUp] = useState(false);
 
-  const publicServers = useSelector((state) => state?.publicServer);
-  const user = useSelector((state) => state?.session.user);
+    const publicServers = useSelector((state) => state?.publicServer);
+    const user = useSelector((state) => state?.session.user);
 
-  useEffect(() => {
-    dispatch(serverActions.findPublicServers());
-  }, [dispatch]);
+    useEffect(() => {
+        dispatch(serverActions.findPublicServers());
+    }, [dispatch]);
 
-  function openModalLogin() {
-    setIsOpenLogin(true);
-  }
+    function openModalLogin() {
+        setIsOpenLogin(true);
+    }
 
-  function openModalSignUp() {
-    setIsOpenSignUp(true);
-  }
+    function openModalSignUp() {
+        setIsOpenSignUp(true);
+    }
 
-  function afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    // subtitle.style.color = '#f00';
-  }
+    function afterOpenModal() {
+        // references are now sync'd and can be accessed.
+        // subtitle.style.color = '#f00';
+    }
 
-  function closeModalLogin() {
-    setIsOpenLogin(false);
-  }
+    function closeModalLogin() {
+        setIsOpenLogin(false);
+    }
 
-  function closeModalSignUp() {
-    setIsOpenSignUp(false);
-  }
+    function closeModalSignUp() {
+        setIsOpenSignUp(false);
+    }
 
-  return (
-    <>
-      <Discover />
-      {/* game page */}
-      <div className="homePage-div__container">
-        <div className="banner-div__container">
-          <img
-            src="https://discord.com/assets/3e0acf6d69894a5d20deb7c513cd1412.svg"
-            alt="home-banner"
-          />
-          <div className="banner-text__container">
-            <h1>Find your community on Discordance</h1>
-            <h2>
-              From gaming, to music, to learning, there's a place for you.
-            </h2>
-            <SearchBar />
-          </div>
-        </div>
-        <div className="main-servers__container">
-          <h1>Featured communities</h1>
-          <div className="servers-containers">
-            {Object.values(publicServers).map((el) => {
-              if (el.category.includes("gaming")) {
-                return (
-                  <div className="server-div__container" key={el?.id}>
-                    <NavLink
-                      onClick={openModalLogin}
-                      to={user ? `servers/${el?.id}` : "/gaming"}
-                    >
-                      <div className="img-div__container">
-                        <img
-                          src={
-                            !el.image_url
-                              ? "https://techjobstour.com/wp-content/uploads/2017/05/appacademylogo.png"
-                              : el.image_url
-                          }
-                          alt="server-profile__image"
-                        />
-                      </div>
-                      <div className="server-div__name"> {el?.name}</div>
-                      <div className="server-div__description">
-                        {" "}
-                        {el?.description}
-                      </div>
-                    </NavLink>
-                  </div>
-                );
-              }
-            })}
-            <Modal
-              isOpen={modalIsOpenLogin}
-              // onAfterOpen={afterOpenModal}
-              onRequestClose={closeModalLogin}
-              style={customStyles}
-              contentLabel="Example Modal"
-            >
-              <LoginForm
-                setIsOpenLogin={setIsOpenLogin}
-                authenticated={authenticated}
-                setAuthenticated={setAuthenticated}
-                closeModalLogin={closeModalLogin}
-                openModalSignUp={openModalSignUp}
-              />
-            </Modal>
-            <Modal
-              isOpen={modalIsOpenSignUp}
-              // onAfterOpen={afterOpenModal}
-              onRequestClose={closeModalSignUp}
-              style={customStyles}
-              contentLabel="Example Modal"
-            >
-              <SignUpForm
-                authenticated={authenticated}
-                setAuthenticated={setAuthenticated}
-                closeModalSignUp={closeModalSignUp}
-                openModalLogin={openModalLogin}
-              />
-            </Modal>
-          </div>
-        </div>
-      </div>
-      {/*ends game page */}
-    </>
-  );
+    return (
+        <>
+            <Discover />
+            {/* game page */}
+            <div className="homePage-div__container">
+                <div className="banner-div__container">
+                    <img
+                        src="https://discord.com/assets/3e0acf6d69894a5d20deb7c513cd1412.svg"
+                        alt="home-banner"
+                    />
+                    <div className="banner-text__container">
+                        <h1>Find your community on Discordance</h1>
+                        <h2>
+                            From gaming, to music, to learning, there's a place
+                            for you.
+                        </h2>
+                        <SearchBar />
+                    </div>
+                </div>
+                <div className="main-servers__container">
+                    <div className="main-servers__header">
+                        <h1>Gaming</h1>
+                        <SportsEsportsIcon />
+                    </div>
+                    <div className="servers-containers">
+                        {Object.values(publicServers).map((el) => {
+                            if (el.category.includes('gaming')) {
+                                return (
+                                    <div
+                                        className="server-div__container"
+                                        key={el?.id}
+                                    >
+                                        <NavLink
+                                            onClick={openModalLogin}
+                                            to={
+                                                user
+                                                    ? `servers/${el?.id}`
+                                                    : '/gaming'
+                                            }
+                                        >
+                                            <div className="img-div__container">
+                                                <img
+                                                    src={
+                                                        !el.image_url
+                                                            ? 'https://techjobstour.com/wp-content/uploads/2017/05/appacademylogo.png'
+                                                            : el.image_url
+                                                    }
+                                                    alt="server-profile__image"
+                                                />
+                                            </div>
+                                            <div className="server-div__name">
+                                                {' '}
+                                                {el?.name}
+                                            </div>
+                                            <div className="server-div__description">
+                                                {' '}
+                                                {el?.description}
+                                            </div>
+                                        </NavLink>
+                                    </div>
+                                );
+                            }
+                        })}
+                        <Modal
+                            isOpen={modalIsOpenLogin}
+                            // onAfterOpen={afterOpenModal}
+                            onRequestClose={closeModalLogin}
+                            style={customStyles}
+                            contentLabel="Example Modal"
+                        >
+                            <LoginForm
+                                setIsOpenLogin={setIsOpenLogin}
+                                authenticated={authenticated}
+                                setAuthenticated={setAuthenticated}
+                                closeModalLogin={closeModalLogin}
+                                openModalSignUp={openModalSignUp}
+                            />
+                        </Modal>
+                        <Modal
+                            isOpen={modalIsOpenSignUp}
+                            // onAfterOpen={afterOpenModal}
+                            onRequestClose={closeModalSignUp}
+                            style={customStyles}
+                            contentLabel="Example Modal"
+                        >
+                            <SignUpForm
+                                authenticated={authenticated}
+                                setAuthenticated={setAuthenticated}
+                                closeModalSignUp={closeModalSignUp}
+                                openModalLogin={openModalLogin}
+                            />
+                        </Modal>
+                    </div>
+                </div>
+            </div>
+            {/*ends game page */}
+        </>
+    );
 }
 
 export default GamePage;
--- react-app/src/components/auth/LoginForm/index.js
+++ react-app/src/components/auth/LoginForm/index.js
@@ -1,123 +1,125 @@
-import React, { useState } from "react";
-import { Redirect } from "react-router-dom";
-import { useHistory } from "react-router-dom";
-import { fetchUserServers } from "../../../store/userInfo";
-import * as sessionActions from "../../../store/session";
-import { useDispatch } from "react-redux";
-import "./LoginForm.css";
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import { fetchUserServers } from '../../../store/userInfo';
+import * as sessionActions from '../../../store/session';
+import { useDispatch } from 'react-redux';
+import './LoginForm.css';
 
 const LoginForm = ({
-  authenticated,
-  setAuthenticated,
-  setIsOpenLogin,
-  openModalSignUp,
-  closeModalLogin,
+    authenticated,
+    setAuthenticated,
+    setIsOpenLogin,
+    openModalSignUp,
+    closeModalLogin,
 }) => {
-  const dispatch = useDispatch();
-  const [errors, setErrors] = useState([]);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const history = useHistory();
-  const onSignUp = (e) => {
-    e.preventDefault();
-    closeModalLogin();
-    openModalSignUp();
-  };
+    const dispatch = useDispatch();
+    const [errors, setErrors] = useState([]);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const history = useHistory();
+    const onSignUp = (e) => {
+        e.preventDefault();
+        closeModalLogin();
+        openModalSignUp();
+    };
 
-  const onLogin = async (e) => {
-    e.preventDefault();
-    const user = await dispatch(sessionActions.login({ email, password }));
+    const onLogin = async (e) => {
+        e.preventDefault();
+        const user = await dispatch(sessionActions.login({ email, password }));
 
-    if (!user.payload.errors) {
-      setIsOpenLogin(false);
-      setAuthenticated(true);
-      dispatch(fetchUserServers(user.payload.id));
+        if (!user.payload.errors) {
+            setIsOpenLogin(false);
+            setAuthenticated(true);
+            dispatch(fetchUserServers(user.payload.id));
 
-      return history.push("/discover");
-    } else {
-      setErrors(user.payload.errors);
+            return history.push('/discover');
+        } else {
+            setErrors(user.payload.errors);
+        }
+    };
+
+    const email1 = 'demo@aa.io';
+    const password1 = 'password';
+
+    const demoUser = (e) => {
+        setEmail(email1);
+        setPassword(password1);
+        dispatch(sessionActions.login({ email: email1, password: password1 }));
+    };
+
+    const updateEmail = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const updatePassword = (e) => {
+        setPassword(e.target.value);
+    };
+
+    // console.log(authenticated);
+    if (authenticated) {
+        return <Redirect to="/" />;
     }
-  };
 
-  const email1 = "demo@aa.io";
-  const password1 = "password";
-
-  const demoUser = (e) => {
-    setEmail(email1);
-    setPassword(password1);
-    dispatch(sessionActions.login({ email: email1, password: password1 }));
-  };
-
-  const updateEmail = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const updatePassword = (e) => {
-    setPassword(e.target.value);
-  };
-
-  // console.log(authenticated);
-  if (authenticated) {
-    return <Redirect to="/" />;
-  }
-
-  return (
-    <div className="LoginModalWrapper">
-      <div className="LoginModalContainer">
-        <div className="LoginModalFormTitleContainer">
-          <div className="LoginModalFormTitle">Welcome Back</div>
+    return (
+        <div className="LoginModalWrapper">
+            <div className="LoginModalContainer">
+                <div className="LoginModalFormTitleContainer">
+                    <div className="LoginModalFormTitle">Welcome Back</div>
+                </div>
+                <form onSubmit={onLogin}>
+                    <div className="LoginErrorModalContainer">
+                        {errors.map((error) => (
+                            <div className="login-errors__container">
+                                {error}
+                            </div>
+                        ))}
+                    </div>
+                    <div className="LoginModalInputContainer">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            name="email"
+                            type="text"
+                            placeholder="Email"
+                            value={email}
+                            onChange={updateEmail}
+                            require
+                        />
+                    </div>
+                    <div className="LoginModalInputContainer">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            name="password"
+                            type="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={updatePassword}
+                            require
+                        />
+                    </div>
+                    <div className="LoginModalButtonContainer">
+                        <button className="LoginModalSubmit" type="submit">
+                            Login
+                        </button>
+                    </div>
+                    <div className="LoginModalButtonContainer">
+                        <button
+                            onClick={(e) => demoUser(e)}
+                            className="LoginModalSubmit"
+                            type="submit"
+                        >
+                            Demo User
+                        </button>
+                    </div>
+                    <div>
+                        <button className="not-a-member" onClick={onSignUp}>
+                            Not a Member? Sign Up
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
-        <form onSubmit={onLogin}>
-          <div className="LoginErrorModalContainer">
-            {errors.map((error) => (
-              <div className="login-errors__container">{error}</div>
-            ))}
-          </div>
-          <div className="LoginModalInputContainer">
-            <label htmlFor="email">Email</label>
-            <input
-              name="email"
-              type="text"
-              placeholder="Email"
-              value={email}
-              onChange={updateEmail}
-              require
-            />
-          </div>
-          <div className="LoginModalInputContainer">
-            <label htmlFor="password">Password</label>
-            <input
-              name="password"
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={updatePassword}
-              require
-            />
-          </div>
-          <div className="LoginModalButtonContainer">
-            <button className="LoginModalSubmit" type="submit">
-              Login
-            </button>
-          </div>
-          <div className="LoginModalButtonContainer">
-            <button
-              onClick={(e) => demoUser(e)}
-              className="LoginModalSubmit"
-              type="submit"
-            >
-              Demo User
-            </button>
-          </div>
-          <div>
-            <button className="not-a-member" onClick={onSignUp}>
-              Not a Member? Sign Up
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default LoginForm;
